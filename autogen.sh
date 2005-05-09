f7fcd60ce4ae48b48936648b607cba8504ca26c3
Ciaran McCreesh <ciaranm@gentoo.org>
2005-05-09 04:16:48 +0000

    switch to automake 1.9
    svn path=/trunk/; revision=86

--- autogen.sh
+++ autogen.sh
@@ -10,10 +10,12 @@ dothis() {
 
 #libtoolize --copy --force --automake
 rm -f config.cache
+export WANT_AUTOMAKE=1.9
+export WANT_AUTOCONF=2.5
 dothis aclocal -I . ${ACLOCAL_FLAGS}
 #dothis autoheader
-dothis automake -a 
 dothis autoconf
+dothis automake -a
 
 echo "Success. Next, run ./configure --help"
 
