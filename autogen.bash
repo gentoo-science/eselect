c43e60cd64b83f64df6bdded0f6cb5d0ef266f4f
Danny van Dyk <kugelfang@gentoo.org>
2006-08-01 22:35:34 +0000

    2006-08-02 Danny van Dyk <kugelfang@gentoo.org>
    	* libs/paludis.bash.in, libs/package-manager.bash.in: Remove
    	paludis-repo(). Fix paludis-envvar(). Finish envvar() unction.
    	* configure.ac: Set version to 1.0.4. 
    	* modules/config.eselect, modules/binutils.eselect: Convert to
    	envvar().
    	* autogen.bash: Fix autogen.bash to be able to use autoconf-2.60.
    
    diffstat:
     ChangeLog                    |   16 ++++++++++++++++
     autogen.bash                 |   32 +++++++++++++++++++++++---------
     configure.ac                 |    2 +-
     libs/package-manager.bash.in |   14 ++++++++------
     libs/paludis.bash.in         |   21 ++-------------------
     modules/binutils.eselect     |    4 ++--
     modules/config.eselect       |    2 +-
     7 files changed, 53 insertions(+), 38 deletions(-)
    
    svn path=/trunk/; revision=287

--- autogen.bash
+++ autogen.bash
@@ -1,28 +1,42 @@
 #!/usr/bin/env bash
+# vim: set sw=4 sts=4 et tw=80 :
+
+if test "xyes" = x"${BASH_VERSION}" ; then
+    echo "This is not bash!"
+    exit 127
+fi
+
+trap 'echo "exiting." ; exit 250' 15
+KILL_PID=$$
 
 run() {
-    echo ">>> $@"
+    echo ">>> $@" 1>&2
     if ! $@ ; then
         echo "oops!" 1>&2
         exit 127
-     fi
+    fi
 }
 
 get() {
-    type ${1}-${2}    &>/dev/null && echo ${1}-${2}    && return
-    type ${1}${2//.}  &>/dev/null && echo ${1}${2//.}  && return
-    type ${1}         &>/dev/null && echo ${1}         && return
-    echo "Could not find ${1} ${2}" 1>&2
-    exit 127
+    local p=${1} v=
+    shift
+
+    for v in ${@} ; do
+        type ${p}-${v}    &>/dev/null && echo ${p}-${v}    && return
+        type ${p}${v//.}  &>/dev/null && echo ${p}${v//.}  && return
+    done
+    type ${p}         &>/dev/null && echo ${p}         && return
+    echo "Could not find ${p}" 1>&2
+    kill $KILL_PID
 }
 
 run mkdir -p config
 run $(get libtoolize 1.5 ) --copy --force --automake
- rm -f config.cache
+rm -f config.cache
 
 run $(get aclocal 1.9 )
 # run $(get autoheader 2.59 )
-WANT_AUTOCONF=2.5 run $(get autoconf 2.59 )
+WANT_AUTOCONF=2.5 run $(get autoconf 2.60 2.59 )
 WANT_AUTOMAKE=1.9 run $(get automake 1.9 ) -a --copy
 
 echo "Success. Now run ./configure --help"
