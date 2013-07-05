5d814839d45230eab0d0d8676eebba16c48ffbad
Ulrich Müller <ulm@gentoo.org>
2013-07-05 22:20:05 +0200

    Update copyright notice.

--- misc/eselect-mode.el
+++ misc/eselect-mode.el
@@ -1,6 +1,6 @@
 ;;; eselect-mode.el --- edit eselect files
 
-;; Copyright 2006-2013 Gentoo Foundation
+;; Copyright (c) 2006-2013 Gentoo Foundation
 
 ;; Author: Matthew Kennedy <mkennedy@gentoo.org>
 ;;	Diego Pettenò <flameeyes@gentoo.org>
@@ -9,18 +9,20 @@
 ;; Maintainer: <emacs@gentoo.org>
 ;; Keywords: languages
 
-;; This file is free software: you can redistribute it and/or modify
+;; This file is part of the 'eselect' tools framework.
+
+;; eselect is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 2 of the License, or
 ;; (at your option) any later version.
 
-;; This file is distributed in the hope that it will be useful,
+;; eselect is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;; along with eselect.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
